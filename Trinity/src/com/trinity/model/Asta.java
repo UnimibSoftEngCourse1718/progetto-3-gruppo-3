c0206fcfe569f3d29a206801b306151a39798b07
Sara Rabbia <s.rabbia@campus.unimib.it>
2018-02-01 18:25:22 +0100

    corretto Asta e i file con gli script

--- Trinity/src/com/trinity/model/Asta.java
+++ Trinity/src/com/trinity/model/Asta.java
@@ -1,11 +1,5 @@
 package com.trinity.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 public abstract class Asta {
 	
