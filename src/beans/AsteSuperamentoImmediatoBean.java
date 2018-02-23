9e03700153c12f48758e92e060c344a02612f0b7
portolesivaleria <v.portolesi@campus.unimib.it>
2018-02-23 01:25:39 +0100

    Risolti errori di visualizzazione

--- src/beans/AsteSuperamentoImmediatoBean.java
+++ src/beans/AsteSuperamentoImmediatoBean.java
@@ -21,6 +21,7 @@ public class AsteSuperamentoImmediatoBean {
 	Oggetto oggetto;
 	UtenteRegistrato venditore;
 	int attiva;
+	Categoria categoria;
 	
 	//metodo che restituisce un arraylist di tutte le aste a superamentoimmediato
 	public ArrayList<AstaSuperamentoImmediato> Aste(){
@@ -33,7 +34,7 @@ public class AsteSuperamentoImmediatoBean {
 			
 			ArrayList<AstaSuperamentoImmediato> aste = new ArrayList <AstaSuperamentoImmediato>();
 			AstaSuperamentoImmediato a = new AstaSuperamentoImmediato();
-			PreparedStatement prep1 = connection.prepareStatement("select * from astasuperamentoimmediato asi inner join utenteregistrato u on asi.venditore = u.idUtente inner join oggetto o on asi.oggetto = o.idOggetto inner join categoria c on o.categoria = c.idCategoria ");
+			PreparedStatement prep1 = connection.prepareStatement("select idAsta, baseAsta, oraInizio, oraFine, timeSlot, o.idOggetto, o.nomeOggetto, o.descrizione, c.idCategoria, c.nomeCategoria, u.idUtente, u.nomeUtente, u.cognomeUtente, u.password, u.email, u.indirizzo, u.numeroCarta, attiva from astasuperamentoimmediato asi inner join utenteregistrato u on asi.venditore = u.idUtente inner join oggetto o on asi.oggetto = o.idOggetto inner join categoria c on o.categoria = c.idCategoria ");
 			ResultSet rs = prep1.executeQuery();
 			while(rs.next()){
 				idAsta=rs.getInt(1);
@@ -41,18 +42,24 @@ public class AsteSuperamentoImmediatoBean {
 				oraInizio = rs.getLong(3);
 				oraFine = rs.getLong(4);
 				timeSlot = rs.getInt(5);
-				oggetto = (Oggetto) rs.getObject(6);
-				venditore = (UtenteRegistrato) rs.getObject(7);
-				attiva = rs.getInt(8);
+				categoria = new Categoria (rs.getInt(9), rs.getString(10));
+				oggetto = new Oggetto(rs.getInt(6), rs.getString(7), rs.getString(8), categoria);
+				
+				venditore = new UtenteRegistrato(rs.getInt(11), rs.getString(12), rs.getString(13), rs.getString(14), rs.getString(15), rs.getString(16), rs.getString(17));
+				attiva = rs.getInt(18);
 				a = new AstaSuperamentoImmediato(idAsta, baseAsta, oraInizio, oraFine, timeSlot, oggetto, venditore, attiva);
 				aste.add(a);
 			}
+			connection.close();
 			return aste;
 			
 		}catch(Exception sqlex){
-			System.out.println("DB error");
+			System.out.println("errore1");
+			System.out.println(sqlex.getMessage());
 			return null;
 		}
+		
+		
 	}
 	
 	//metodo che restituisce l'asta con dato id
@@ -72,7 +79,8 @@ public class AsteSuperamentoImmediatoBean {
 			return asta;
 			
 		}catch(Exception sqlex){
-			System.out.println("DB error");
+			System.out.println("errore2");
+			System.out.println(sqlex.getMessage());
 			return null;
 		}
 	}
